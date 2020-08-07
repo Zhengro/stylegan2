417c3799fb60cccba78e1d1368641a4cf5e7d824
Zhengro <yizhengrong1995@gmail.com>
2020-08-07 16:24:39 +0000

    Modified run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -94,7 +94,7 @@ def style_mixing_example(network_pkl, row_seeds, col_seeds, truncation_psi, col_
 #----------------------------------------------------------------------------
 
 
-def style_mixing_moving(network_pkl, row_seeds, col_seeds, truncation_psi, minibatch_size=4):
+def style_mixing_gradual_change(network_pkl, row_seeds, col_seeds, truncation_psi, minibatch_size=4):
     col_styles = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 15, 17]
     print('col_styles: {}'.format(col_styles))
     print('Loading networks from "%s"...' % network_pkl)
@@ -165,49 +165,6 @@ def style_mixing_noise(network_pkl, row_seeds, col_seeds, col_styles, truncation
 #----------------------------------------------------------------------------
 
 
-def style_mixing_two(network_pkl, row_seeds, col_seeds, col_styles, truncation_psi, minibatch_size=4):
-    import time
-    print('col_styles: {}'.format(col_styles))
-    print('Loading networks from "%s"...' % network_pkl)
-    start = time.time()
-    _G, _D, Gs = pretrained_networks.load_networks(network_pkl)
-    w_avg = Gs.get_var('dlatent_avg')  # [component]
-    print('Load network: {} s'.format(time.time()-start))
-
-    Gs_syn_kwargs = dnnlib.EasyDict()
-    Gs_syn_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-    Gs_syn_kwargs.randomize_noise = False
-    Gs_syn_kwargs.minibatch_size = minibatch_size
-
-    print('Generating W vectors...')
-    start = time.time()
-    all_seeds = list(set(row_seeds + col_seeds))
-    all_z = np.stack([np.random.RandomState(seed).randn(*Gs.input_shape[1:]) for seed in all_seeds])  # [minibatch, component]
-    all_w = Gs.components.mapping.run(all_z, None)  # [minibatch, layer, component]
-    all_w = w_avg + (all_w - w_avg) * truncation_psi  # [minibatch, layer, component]
-    w_dict = {seed: w for seed, w in zip(all_seeds, list(all_w))}  # [layer, component]
-    image_dict = {}
-    print('Generating W: {} s'.format(time.time()-start))
-
-    print('Generating style-mixed images...')
-    start = time.time()
-    for row_seed in row_seeds:
-        for col_seed in col_seeds:
-            w = w_dict[row_seed].copy()
-            w[col_styles] = w_dict[col_seed][col_styles]
-            image = Gs.components.synthesis.run(w[np.newaxis], **Gs_syn_kwargs)[0]  # [height, width, channel]
-            image_dict[(row_seed, col_seed)] = image
-    print('Generating an image: {} s'.format(time.time()-start))
-
-    print('Saving images...')
-    start = time.time()
-    for (row_seed, col_seed), image in image_dict.items():
-        PIL.Image.fromarray(image, 'RGB').save(dnnlib.make_run_dir_path('%d-%d %d-%d.png' % (row_seed, col_seeds[0], col_styles[0], col_styles[-1])))
-    print('Saving an image: {} s'.format(time.time()-start))
-
-#----------------------------------------------------------------------------
-
-
 def style_mixing_multiple(network_pkl, row_seeds, col_seeds, truncation_psi, minibatch_size=4):
     col_styles = [[6, 7], [0, 3]]
     print('col_styles: {}'.format(col_styles))
@@ -257,18 +214,21 @@ def _parse_num_range(s):
 
 
 _examples = '''examples:
+    
+    # Example of image generation
+    python run_generator.py generate-images --network=stylegan2-ffhq-config-f.pkl --seeds=0-9 --truncation-psi=1.0
+    
+    # Example of style mixing
+    python run_generator.py style-mixing-example --network=stylegan2-ffhq-config-f.pkl --row-seeds=85,100,75,458,1500 --col-seeds=55,821,1789,293 --truncation-psi=1.0
 
-  # Generate ffhq uncurated images (matches paper Figure 12)
-  python %(prog)s generate-images --network=gdrive:networks/stylegan2-ffhq-config-f.pkl --seeds=6600-6625 --truncation-psi=0.5
+    # Example of gradual change
+    python run_generator.py style-mixing-gradual-change --network=stylegan2-ffhq-config-f.pkl --row-seeds=100 --col-seeds=85 --truncation-psi=1.0
 
-  # Generate ffhq curated images (matches paper Figure 11)
-  python %(prog)s generate-images --network=gdrive:networks/stylegan2-ffhq-config-f.pkl --seeds=66,230,389,1518 --truncation-psi=1.0
-
-  # Generate uncurated car images (matches paper Figure 12)
-  python %(prog)s generate-images --network=gdrive:networks/stylegan2-car-config-f.pkl --seeds=6000-6025 --truncation-psi=0.5
-
-  # Generate style mixing example (matches style mixing video clip)
-  python %(prog)s style-mixing-example --network=gdrive:networks/stylegan2-ffhq-config-f.pkl --row-seeds=85,100,75,458,1500 --col-seeds=55,821,1789,293 --truncation-psi=1.0
+    # Example of noise effect
+    python run_generator.py style-mixing-noise --network=stylegan2-ffhq-config-f.pkl --row-seeds=100 --col-seeds=85 --col-styles=0-1 --randomize-noise True --truncation-psi=1.0
+    
+    # Example of style mixing of three references
+    python run_generator.py style-mixing-multiple --network=stylegan2-ffhq-config-f.pkl --row-seeds=100 --col-seeds=85,20 --truncation-psi=1.0
 '''
 
 #----------------------------------------------------------------------------
@@ -291,7 +251,7 @@ Run 'python %(prog)s <subcommand> --help' for subcommand help.''',
     parser_generate_images.add_argument('--truncation-psi', type=float, help='Truncation psi (default: %(default)s)', default=0.5)
     parser_generate_images.add_argument('--result-dir', help='Root directory for run results (default: %(default)s)', default='results', metavar='DIR')
 
-    parser_style_mixing_example = subparsers.add_parser('style-mixing-example', help='Generate style mixing video')
+    parser_style_mixing_example = subparsers.add_parser('style-mixing-example', help='Generate style mixing')
     parser_style_mixing_example.add_argument('--network', help='Network pickle filename', dest='network_pkl', required=True)
     parser_style_mixing_example.add_argument('--row-seeds', type=_parse_num_range, help='Random seeds to use for image rows', required=True)
     parser_style_mixing_example.add_argument('--col-seeds', type=_parse_num_range, help='Random seeds to use for image columns', required=True)
@@ -299,12 +259,12 @@ Run 'python %(prog)s <subcommand> --help' for subcommand help.''',
     parser_style_mixing_example.add_argument('--truncation-psi', type=float, help='Truncation psi (default: %(default)s)', default=0.5)
     parser_style_mixing_example.add_argument('--result-dir', help='Root directory for run results (default: %(default)s)', default='results', metavar='DIR')
 
-    parser_style_mixing_moving = subparsers.add_parser('style-mixing-moving', help='Generate gradual change')
-    parser_style_mixing_moving.add_argument('--network', help='Network pickle filename', dest='network_pkl', required=True)
-    parser_style_mixing_moving.add_argument('--row-seeds', type=_parse_num_range, help='Random seeds to use for image rows', required=True)
-    parser_style_mixing_moving.add_argument('--col-seeds', type=_parse_num_range, help='Random seeds to use for image columns', required=True)
-    parser_style_mixing_moving.add_argument('--truncation-psi', type=float, help='Truncation psi (default: %(default)s)', default=0.5)
-    parser_style_mixing_moving.add_argument('--result-dir', help='Root directory for run results (default: %(default)s)', default='results', metavar='DIR')
+    parser_style_mixing_gradual_change = subparsers.add_parser('style-mixing-gradual-change', help='Generate gradual change')
+    parser_style_mixing_gradual_change.add_argument('--network', help='Network pickle filename', dest='network_pkl', required=True)
+    parser_style_mixing_gradual_change.add_argument('--row-seeds', type=_parse_num_range, help='Random seeds to use for image rows', required=True)
+    parser_style_mixing_gradual_change.add_argument('--col-seeds', type=_parse_num_range, help='Random seeds to use for image columns', required=True)
+    parser_style_mixing_gradual_change.add_argument('--truncation-psi', type=float, help='Truncation psi (default: %(default)s)', default=0.5)
+    parser_style_mixing_gradual_change.add_argument('--result-dir', help='Root directory for run results (default: %(default)s)', default='results', metavar='DIR')
 
     parser_style_mixing_noise = subparsers.add_parser('style-mixing-noise', help='Pose noises')
     parser_style_mixing_noise.add_argument('--network', help='Network pickle filename', dest='network_pkl', required=True)
@@ -314,14 +274,6 @@ Run 'python %(prog)s <subcommand> --help' for subcommand help.''',
     parser_style_mixing_noise.add_argument('--truncation-psi', type=float, help='Truncation psi (default: %(default)s)', default=0.5)
     parser_style_mixing_noise.add_argument('--randomize-noise', type=bool, help='Randomize noise (default: %(default)s)', default=False)
     parser_style_mixing_noise.add_argument('--result-dir', help='Root directory for run results (default: %(default)s)', default='results', metavar='DIR')
-
-    parser_style_mixing_two = subparsers.add_parser('style-mixing-two', help='Generate from two references')
-    parser_style_mixing_two.add_argument('--network', help='Network pickle filename', dest='network_pkl', required=True)
-    parser_style_mixing_two.add_argument('--row-seeds', type=_parse_num_range, help='Random seeds to use for image rows', required=True)
-    parser_style_mixing_two.add_argument('--col-seeds', type=_parse_num_range, help='Random seeds to use for image columns', required=True)
-    parser_style_mixing_two.add_argument('--col-styles', type=_parse_num_range, help='Style layer range (default: %(default)s)', default='0-6')
-    parser_style_mixing_two.add_argument('--truncation-psi', type=float, help='Truncation psi (default: %(default)s)', default=0.5)
-    parser_style_mixing_two.add_argument('--result-dir', help='Root directory for run results (default: %(default)s)', default='results', metavar='DIR')
 
     parser_style_mixing_multiple = subparsers.add_parser('style-mixing-multiple', help='Generate from multiple references')
     parser_style_mixing_multiple.add_argument('--network', help='Network pickle filename', dest='network_pkl', required=True)
@@ -348,9 +300,8 @@ Run 'python %(prog)s <subcommand> --help' for subcommand help.''',
     func_name_map = {
         'generate-images': 'run_generator.generate_images',
         'style-mixing-example': 'run_generator.style_mixing_example',
-        'style-mixing-moving': 'run_generator.style_mixing_moving',
+        'style-mixing-gradual-change': 'run_generator.style_mixing_gradual_change',
         'style-mixing-noise': 'run_generator.style_mixing_noise',
-        'style-mixing-two': 'run_generator.style_mixing_two',
         'style-mixing-multiple': 'run_generator.style_mixing_multiple'
     }
     dnnlib.submit_run(sc, func_name_map[subcmd], **kwargs)
@@ -360,5 +311,3 @@ Run 'python %(prog)s <subcommand> --help' for subcommand help.''',
 
 if __name__ == "__main__":
     main()
-
-#----------------------------------------------------------------------------
